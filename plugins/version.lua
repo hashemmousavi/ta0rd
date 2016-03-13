231c9f1f9bd81be83b3dc5f143454099c7693c42
MaStEr FeRi <feri.sys13@Gmail.com>
2016-03-13 18:02:54 +0330

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,26 @@
+--------------------------------------------------
+--                                              --
+--            Developer: @Ferisystem            --
+--             Support: @TardTeamCh             --
+--                                              --
+--------------------------------------------------
+
+do
+
+function run(msg, matches)
+  return '⚡️ Xamarin Plus Bot v1.0\n    \n  📢 Channel : @TardTeamCh\n  👤 Admin : @Ferisystem\n  \n  🙏 Special Thanks to :\n  \n        @mahdi17177' 
+end
+
+return {
+  patterns = {
+    "^[!#/]teletad+$",
+    "^teletad+$",
+    "^[!#/]teletard$",
+    "^teletard$",
+    "^[!#/]version$",
+    "^version$"
+  }, 
+  run = run 
+}
+
+end
